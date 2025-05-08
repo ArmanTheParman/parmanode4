531135e900cc58e3f0a0ca035fffb189fb018c1f
Parman <armantheparman@protonmail.com>
2025-05-08 21:29:29 +1000

    a

---
+++ bash/config/parmanode_variables.sh
@@ -0,0 +1,52 @@
+function parmanode_variables {
+
+#system
+export dn=/dev/null
+
+#OS specific
+if [[ $(uname) == "Linux" ]] ; then
+    export macprefix=""
+    export parmanode_drive="/media/$USER/parmanode"
+    export bashrc="$HOME/.bashrc"
+    export torrc="/etc/tor/torrc"
+    export varlibtor="/var/lib/tor"
+
+elif [[ $(uname) == "Darwin" ]] ; then
+    export macprefix="$(brew --prefix 2>/dev/null)" ; if [[ -z $macprefix ]] ; then export macprefix="/usr/local" ; fi
+    export parmanode_drive="/Volumes/parmanode"
+    export bashrc="$HOME/.zshrc"
+    export torrc="$macprefix/etc/tor/torrc"
+    export varlibtor="$macprefix/var/lib/tor"
+fi
+
+#parmanode4 dirs
+export pp=$HOME/parman_programs
+export dp=$HOME/.parmanode4
+export hp=$HOME/parmanode4
+export pn=$pp/parmanode4
+export pd=$parmanode_drive
+
+#parmanode4 confs
+export pdc=$dp/parmanode.conf
+export pc=$dp/parmanode.conf
+export ic=$dp/installed.conf
+export oc=$dp/overview.conf
+export hm=$dp/hide_messages.conf
+
+#parmanode logs
+export debug=$dp/debug.log
+
+#Bitcoin
+export b=$HOME/.bitcoin
+export bc=$db/bitcoin.conf
+
+#Fulcrum
+export fc=$HOME/.fulcrum/fulcrum.conf
+
+#Nostr 
+export nk=$dp/.nostr_keys/nostr_keys.txt
+export nkd=$dp/.nostr_keys
+
+#Nginx
+export parmaviewnginx="$macprefix/etc/nginx/conf.d/parmaview.conf"
+}
--- install_parmanode4.sh
+++ install_parmanode4.sh
@@ -2,39 +2,31 @@
 
 if [[ $1 == x ]] ; then set -x ; fi
 
-clear
 heading "Install Parmanode..."
-body "
-##################################################
 
-Good call on installing Parmanode. This 
-installation will add a few directories and files
-here and there, and also install some programs
-that are necessary for Parmanode to function. You
+body "Good call on installing Parmanode. This installation will add a few directories and files \
+here and there, and also install some programs that are necessary for Parmanode to function. You \
 will be asked to confirm on the next page.
 
-If Parmanode3 is installed on your machine, 
-Parmanode4 will detect that, and any programs
-you have installed with it, and migrate them
-across.
+If Parmanode3 is installed on your machine, Parmanode4 will detect that, and any programs you have \
+installed with it, and migrate them across.
 
-Once that is done, Parmanode4 will Remove
-Parmanode3 for you.
+Once that is done, Parmanode4 will Remove Parmanode3 for you."
 
-##################################################
-"
 choose "continue" "exit"
 
 #check files and directories exist
-[[ -e $HOME/parman_programs ]] || mkdir -p $HOME/parman_programs
-[[ -e $HOME/.parmanode4 ]]     || mkdir -p $HOME/.parmanode4
+[[ -e $HOME/parman_programs ]]                || mkdir -p $HOME/parman_programs
+[[ -e $HOME/.parmanode4 ]]                    || mkdir -p $HOME/.parmanode4
 [[ -e $HOME/.parmanode4/installed.conf ]]     || mkdir -p $HOME/.parmanode4/installed.conf
 [[ -e $HOME/.parmanode4/parmanode.conf ]]     || mkdir -p $HOME/.parmanode4/parmanode.conf
 [[ -e $HOME/.parmanode4/parmanode.log ]]      || mkdir -p $HOME/.parmanode4/parmanode.log
 [[ -e $HOME/.parmanode4/debug.log ]]          || mkdir -p $HOME/.parmanode4/debug.log
+[[ -e $HOME/.parmanode4/temp ]]               || mkdir -p $HOME/.parmanode4/temp
 
 
-#check programs exist, and install
+#check programs exist, and print form...
+#reponse from form creates json. Each install command below checks the json.
 install_docker || sww
 restart_after_docker_install
 intsall_brew || sww
@@ -45,4 +37,29 @@ install_python || sww
 install_pip || sww
 install_websockets || sww
 bash_version_check || sww
-install_parmashedll || sww
+install_parmashell || sww
+
+cat <<EOF
+
+Docker    Installed?
+Brew      N/A
+Git
+Curl
+gsed
+python
+pip
+websockets
+jq
+netcat-traditional
+vim
+net-tools
+unzip
+tmux
+ssh
+tor
+ufw
+mdadm
+e2fsprogs
+tune2fs
+fuse3
+libfuse2
