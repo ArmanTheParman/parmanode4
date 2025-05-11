c3032ad16e5bd8aba9fada89e0c75c6944766dc1
Parman <armantheparman@protonmail.com>
2025-05-11 22:48:44 +1000

    a

--- run_parmanode4.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $1 == x ]] ; then set -x ; fi
-
-main $@
-
-exit
--- uninstall_parmanode4.sh
+++ uninstall_parmanode4.sh
@@ -1,22 +1,26 @@
-function uninstall_parmanode4 {
+#!/bin/bash
+
+source $HOME/parman_programs/parmanode4/bash/config/parmanode_variables.sh
 
 yesorno "Are you sure you want to uninstall Parmanode4? You will first have the option to remove installed programs" || return 1
 
-uninstall_apps || return 1
+#uninstall_apps || return 1
 
-sudo rm -rf $dp
-sudo rm -rf $pn4
+sudo rm -rf $HOME/.parmanode4
+sudo rm -rf $HOME/parman_programs/parmanode4
 
 #uninstall_cgi
     sudo umount $wwwparmaviewdir && sudo rm -rf $wwwparmaviewdir
     sudo rm -rf $parmaviewnginx
-    [[ $(uname) == "Darwin" ]] || { 
-        sudo systemctl restart nginx 
-        sudo systemctl disable fcgiwrap >$dn 
+    if [[ $(uname) == "Linux" ]] ; then 
         sudo rm -rf /etc/systemd/system/fcgiwrap.service.d 
+        sudo systemctl disable fcgiwrap >$dn 
         sudo systemctl daemon-reload
-    }
+        sudo systemctl restart nginx 
+    elif [[ $(uname == "Darwin" ]] ; then 
+        sudo rm -rf $macprevix
 
+        
 #clean up bashrc/zshrc
 #unmount
 #clearn up crontab
