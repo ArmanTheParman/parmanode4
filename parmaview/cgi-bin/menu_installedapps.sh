9daa11bc02877389b2d4da56c9f336c36a0f5f4c
Parman <armantheparman@protonmail.com>
2025-05-14 03:24:52 +1000

    a

--- parmaview/cgi-bin/menu_installedapps.sh
+++ parmaview/cgi-bin/menu_installedapps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Content-Type: application/json"
+echo "Content-Type: text/plain"
 echo ""
 
 gsed -n '/<body>/,/<\/body>/p' $HOME/parman_programs/parmanode4/parmaview/menu_installedapps.html | sed '1d;$d;'
