b87c8538ef983df87a1f93a66f79012e9c1b4492
Parman <armantheparman@protonmail.com>
2025-05-14 04:04:42 +1000

    a

--- parmaview/cgi-bin/menu_installapps.sh
+++ parmaview/cgi-bin/menu_installapps.sh
@@ -2,5 +2,4 @@
 echo "Content-Type: text/plain"
 echo ""
 
-#gsed -n '/<body>/,/<\/body>/p' $HOME/parman_programs/parmanode4/parmaview/menu_installapps.html | sed '1d;$d;'
-echo "TEST"
+gsed -n '/<body>/,/<\/body>/p' $HOME/parman_programs/parmanode4/parmaview/menu_installapps.html | sed '1d;$d;'
