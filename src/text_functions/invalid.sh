bbb5b27ef9f02f040e38e68dc8656c7282a4cc2b
Parman <armantheparman@protonmail.com>
2025-05-28 01:59:59 +1000

    a

--- src/text_functions/invalid.sh
+++ src/text_functions/invalid.sh
@@ -1,6 +1,9 @@
 function invalid {
 clear
-echo -e " ${yellow}Invalid choice."
-sleep 1.5
+enter_continue "Invalid choice, hit enter to try again or q to quit."
+if [[ $enter_cont == "q" ]] ; then
+exit 0
+fi
+
 return 0
 }
