a54525f1fe5b69eb7514f170d2bd707d894b4a10
Parman <armantheparman@protonmail.com>
2025-05-29 18:40:52 +1000

    a

--- scripts/background_electrs_download.sh
+++ scripts/background_electrs_download.sh
@@ -4,27 +4,7 @@
 #Downloads Electrs and compiles
 # if pruning, electrs will still download and compile, but won't run - need warning to user.
 
-function make_socat_service {
 
-echo "[Unit]
-Description=Socat SSL to TCP Forwarding Service
-After=network.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$HOME/.electrs/cert.pem,key=$HOME/.electrs/key.pem,verify=0 TCP:127.0.0.1:50005
-Restart=on-failure
-RestartSec=10
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-" | sudo tee /etc/systemd/system/parmanode/socat.service >$dn 2>&1
-
-sudo systemctl daemon-reload >$dn 2>&1
-sudo systemctl enable socat.service >$dn 2>&1
-sudo systemctl start socat.service >$dn 2>&1
-}
 
 #Begin...
 
@@ -33,7 +13,8 @@ export electrsversion="v0.10.9"
 if jq '.parmanode' $pc | grep "elects_downloaded" $pj | grep -q true ; then exit ; fi
 jq '.parmanode += {electrs_download: started}' $pj >$pj.tmp && mv $pj.tmp $pj
 
-make_socat_service
+source $HOME/parman_programs/parmanode4/src/services/make_socat_service
+
 build_dependencies_electrs 
 download_electrs 
 compile_electrs 
---
+++ src/services/make_socat_service.sh
@@ -0,0 +1,21 @@
+function make_socat_service {
+
+echo "[Unit]
+Description=Socat SSL to TCP Forwarding Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$HOME/.electrs/cert.pem,key=$HOME/.electrs/key.pem,verify=0 TCP:127.0.0.1:50005
+Restart=on-failure
+RestartSec=10
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/parmanode/socat.service >$dn 2>&1
+
+sudo ln -s /etc/systemd/system/parmanode/socat.service /etc/systemd/system/parmanode.target.wants/socat.service >$dn 2>&1
+sudo systemctl daemon-reload >$dn 2>&1
+sudo systemctl enable --now socat.service >$dn 2>&1
+}
