0c070a109689dcb3ea61de2630bf86d4476f18c3
Parman <armantheparman@protonmail.com>
2025-07-17 16:15:44 +1000

    a

--- src/text_functions/enter_continue.sh
+++ src/text_functions/enter_continue.sh
@@ -7,18 +7,11 @@ read enter_cont </dev/tty
 export enter_cont
 
 case $enter_cont in
-q) exit ;;
-tmux) tmux ;;
-debugon)
-export debug=1
-fi
-debugoff)
-export debug=0
-;;
+q) exit ;; tmux) tmux ;; debugon) export debug=1 fi ;; debugoff) export debug=0 ;; x)  set +x ;;
 d)
     if [[ $debug == 1 ]] ; then export debug=0 ; fi
     if [[ $debug == 0 ]] ; then export debug=1 ; fi
     ;;
-x)  set +x ;;
 esac
+return 0
 }
